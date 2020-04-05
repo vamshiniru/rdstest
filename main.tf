bf362fe9b2c29c1e2bf76b9e30f2ae784f76ff45
vamshiniru <57339237+vamshiniru@users.noreply.github.com>
2020-04-06 02:04:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_db_instance" "default" {
   name                   = "${var.db_name}"
   username               = "${var.username}"
   password               = "${var.password}"
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  vpc_security_group_ids =  "sg-027897c747be68c2e"
   db_subnet_group_name   = "${aws_db_subnet_group.default.id}"
 }
 
