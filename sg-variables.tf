04172cb9374c5d5978de9489bd08bacfc9be8a2f
vamshiniru <57339237+vamshiniru@users.noreply.github.com>
2020-04-06 01:52:53 +0530

    Create sg-variables.tf

---
+++ sg-variables.tf
@@ -0,0 +1,12 @@
+variable "cidr_blocks" {
+  default     = "0.0.0.0/0"
+  description = "CIDR for sg"
+}
+
+variable "sg_name" {
+  default     = "rds_sg"
+  description = "Tag Name for sg"
+}
+variable "vpc_id" {
+  description = "Your VPC ID"
+}
