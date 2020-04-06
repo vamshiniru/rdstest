825f5f05bc2c4df8c6e43f4a2ea68b4af12c750d
vamshiniru <57339237+vamshiniru@users.noreply.github.com>
2020-04-06 14:54:20 +0530

    Create subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,25 @@
+ resource "aws_vpc" "vpc_id" {
+  cidr_block       = "10.0.0.0/16"
+  tags = {
+    Name = "vpc_id"
+  }
+} 
+resource "aws_subnet" "subnet_1" {
+  vpc_id            = "${var.vpc_id}"
+  cidr_block        = "${var.subnet_1_cidr}"
+  availability_zone = "${var.az_1}"
+
+  tags = {
+    Name = "main_subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id            = "${var.vpc_id}"
+  cidr_block        = "${var.subnet_2_cidr}"
+  availability_zone = "${var.az_2}"
+
+  tags = {
+    Name = "main_subnet2"
+  }
+}
